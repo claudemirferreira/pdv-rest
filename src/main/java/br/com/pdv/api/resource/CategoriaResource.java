6fab3df8bf38b79bc9778408d2f39257402cab4d
Claudemir Ramos Ferreira (BR-MIS) <bb8ga118@cal-comp.com.br>
2019-04-05 09:42:02 -0400

    criei a tabela na base

--- src/main/java/br/com/pdv/api/resource/CategoriaResource.java
+++ src/main/java/br/com/pdv/api/resource/CategoriaResource.java
@@ -1,17 +1,29 @@
 package br.com.pdv.api.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pdv.api.model.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 
-		return "Rest esta funcionando";
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(1, "Escritório");
+
+		List<Categoria> list = new ArrayList<Categoria>();
+		list.add(c1);
+		list.add(c2);
+
+		return list;
 	}
 
 }
