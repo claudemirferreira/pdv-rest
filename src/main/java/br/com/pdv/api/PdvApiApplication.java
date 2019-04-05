5d9807a987cec72fdb11697c3bf64c60a014bbc8
Claudemir Ramos Ferreira (BR-MIS) <bb8ga118@cal-comp.com.br>
2019-04-05 10:17:53 -0400

    implementando operacao de instaciacao

--- src/main/java/br/com/pdv/api/PdvApiApplication.java
+++ src/main/java/br/com/pdv/api/PdvApiApplication.java
@@ -1,14 +1,34 @@
 package br.com.pdv.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.pdv.api.model.domain.Categoria;
+import br.com.pdv.api.model.repository.CategoriaRepository;
 
 @SpringBootApplication
-public class PdvApiApplication {
+public class PdvApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdvApiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritorio");
+		repo.saveAll(Arrays.asList(c1,c2));
+		
+	}
+	
+	
 
 }
